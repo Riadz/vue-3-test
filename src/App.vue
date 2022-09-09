b06cf2b0ff34c879721fc6ecda2fb51ef68da58a
Riadz <riad.hachemane@gmail.com>
2022-09-09 18:45:07 +0100

    added main layout + emails array

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
 <template>
-  <h1>Hello world</h1>
+  <div class="container">
+    <aside class="container__aside"></aside>
+    <main class="container__main">
+      <div>{{ emails }}</div>
+    </main>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { getEmails } from './emails';
+
+const emails = ref<any[]>(getEmails());
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  font-family: 'Segoe UI', sans-serif;
+}
+
+.container {
+  display: flex;
+  min-height: 100vh;
+
+  &__aside {
+    width: 25%;
+    background-color: #373636;
+    padding: 2rem 1.8rem;
+  }
+  &__main {
+    width: 75%;
+    padding: 3rem 5rem;
+  }
+}
+</style>
---
+++ src/emails.ts
@@ -0,0 +1,31 @@
+const emails = [
+  {
+    id: 1,
+    title: 'Your 7 figure plan goes',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi placeat repellendus repudiandae, delectus quaerat nemo dolorem illum sint neque quidem doloremque! Facere voluptate reprehenderit alias quam, pariatur id dolores tempora?',
+    isSelected: false,
+    isRead: false,
+    isArchived: false,
+  },
+  {
+    id: 2,
+    title: '[WEEKEND ONLY] Get Now 🔥',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi placeat repellendus repudiandae, delectus quaerat nemo dolorem illum sint neque quidem doloremque! Facere voluptate reprehenderit alias quam, pariatur id dolores tempora?',
+    isSelected: false,
+    isRead: false,
+    isArchived: false,
+  },
+  {
+    id: 3,
+    title: 'Something just expired 😨',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi placeat repellendus repudiandae, delectus quaerat nemo dolorem illum sint neque quidem doloremque! Facere voluptate reprehenderit alias quam, pariatur id dolores tempora?',
+    isSelected: false,
+    isRead: false,
+    isArchived: false,
+  },
+];
+
+export const getEmails = () => emails;
