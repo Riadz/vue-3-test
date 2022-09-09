18fc37982b35f185fbc3eb332626c01169411a31
Riadz <riad.hachemane@gmail.com>
2022-09-09 19:09:01 +0100

    finished sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="container">
-    <aside class="container__aside"></aside>
-    <main class="container__main">
+    <aside class="container__aside aside">
+      <button class="btn aside__btn" @click="() => (page = 'inbox')">
+        Inbox ()
+      </button>
+      <button class="btn aside__btn" @click="() => (page = 'archive')">
+        Archive ()
+      </button>
+
+      <button class="btn aside__btn aside__btn--bottom">Logout</button>
+    </aside>
+    <main class="container__main main">
+      <div>{{ page }}</div>
       <div>{{ emails }}</div>
     </main>
   </div>
@@ -11,7 +21,8 @@
 import { ref } from 'vue';
 import { getEmails } from './emails';
 
-const emails = ref<any[]>(getEmails());
+const page = ref('inbox');
+const emails = ref(getEmails());
 </script>
 
 <style lang="scss">
@@ -31,7 +42,33 @@ body {
   }
   &__main {
     width: 75%;
-    padding: 3rem 5rem;
+    padding: 2rem 5rem;
   }
+}
+
+.aside {
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+
+  &__btn {
+    text-align: start;
+
+    &--bottom {
+      margin-top: auto;
+    }
+  }
+}
+
+//
+.btn {
+  cursor: pointer;
+  color: #000;
+  padding: 0.6rem 1.9rem;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  border: 1px solid #c9c9c9;
+  font-weight: 600;
+  font-size: 1rem;
 }
 </style>
