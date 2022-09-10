d6e18043196eede2146ac3d283e3100ff84283b6
Riadz <riad.hachemane@gmail.com>
2022-09-10 19:51:03 +0100

    archive/index pages

---
+++ src/pages/archive.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+import EmailList from '@/components/EmailList.vue';
+import { emails as allEmails } from '@/state/emails';
+
+const emails = computed(() =>
+  allEmails.value.filter((email) => email.isArchived),
+);
+const selectedCount = ref(0);
+</script>
+
+<template>
+  <div>
+    <div class="main__title">Archive</div>
+    <div class="main__selected">Emails Selected ({{ selectedCount }})</div>
+
+    <EmailList :emails="emails" v-model:selectedCount="selectedCount" />
+  </div>
+</template>
