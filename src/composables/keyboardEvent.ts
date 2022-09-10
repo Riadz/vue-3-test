c82974292c9f1b9c4f5514ccabe550f2c4826f06
Riadz <riad.hachemane@gmail.com>
2022-09-10 20:36:34 +0100

    keyboard events composable

---
+++ src/composables/keyboardEvent.ts
@@ -0,0 +1,16 @@
+import { unref, onMounted, onUnmounted, Ref, ref } from 'vue';
+
+export function useKeyboard(
+  key: string,
+  callback: Function,
+  condition: Ref<boolean> = ref(true),
+) {
+  function event(e: KeyboardEvent) {
+    if (e.key !== key || !condition.value) return;
+
+    callback();
+  }
+
+  onMounted(() => window.addEventListener('keyup', event));
+  onUnmounted(() => window.removeEventListener('keyup', event));
+}
