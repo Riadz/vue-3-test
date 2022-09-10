6b62238893ee306dd74572e2e8f72916142eefb8
Riadz <riad.hachemane@gmail.com>
2022-09-10 18:45:54 +0100

    extracted sidebar

---
+++ src/components/MainSidebar.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { useEmails } from '@/composables/emails';
+
+const { emailsInboxCount, emailsArchivedCount } = useEmails();
+</script>
+
+<template>
+  <div class="sidebar">
+    <RouterLink to="/inbox" class="btn sidebar__btn">
+      Inbox ({{ emailsInboxCount }})
+    </RouterLink>
+    <RouterLink to="/archive" class="btn sidebar__btn">
+      Archive ({{ emailsArchivedCount }})
+    </RouterLink>
+
+    <button class="btn sidebar__btn sidebar__btn--bottom">Logout</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  height: 100%;
+  padding: 2rem 1.8rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+
+  &__btn {
+    text-align: start;
+
+    &--bottom {
+      margin-top: auto;
+    }
+  }
+}
+</style>
