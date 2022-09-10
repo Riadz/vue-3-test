2a81d02398c69a4c25dce8e0eb071dc6ff31a131
Riadz <riad.hachemane@gmail.com>
2022-09-10 19:02:04 +0100

    minor change

--- src/components/MainSidebar.vue
+++ src/components/MainSidebar.vue
@@ -1,7 +1,5 @@
 <script lang="ts" setup>
-import { useEmails } from '@/composables/emails';
-
-const { emailsInboxCount, emailsArchivedCount } = useEmails();
+import { emailsInboxCount, emailsArchivedCount } from '@/composables/emails';
 </script>
 
 <template>
